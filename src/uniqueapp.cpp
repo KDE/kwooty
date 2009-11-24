cf2264b670809da863c07a6b4991d5c2bd7448d1
Xavier Lefage <xavier.kwooty@gmail.com>
2009-11-24 19:54:14 +0100

    Handle files open from external apps

--- src/uniqueapp.cpp
+++ src/uniqueapp.cpp
@@ -1,10 +1,32 @@
+/***************************************************************************
+ *   Copyright (C) 2009 by Xavier Lefage                                   *
+ *   xavier.kwooty@gmail.com                                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
 #include "uniqueapp.h"
 
-#include "mainwindow.h"
 #include <KIcon>
 #include <KCmdLineArgs>
 #include <KDebug>
 #include <KUrl>
+
+#include "mainwindow.h"
+
 
 UniqueApp::UniqueApp() : KUniqueApplication()
 {
@@ -40,7 +62,7 @@ int UniqueApp::newInstance()
         // open nzb files set as argument :
         for (int i = 0; i < args->count(); i++) {
 
-            this->mainWindow->openFileSilently(args->url(i));
+            this->mainWindow->openFileByExternalApp(args->url(i));
 
         }
 
