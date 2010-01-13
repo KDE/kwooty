134eca72a7a2eefd6fd8bcc5270aa8ed3b4bb675
xavier.kwooty <xavier.kwooty@gmail.com>
2010-01-14 00:36:07 +0100

    added gpl header

--- src/itemchildrenmanager.h
+++ src/itemchildrenmanager.h
@@ -1,13 +1,34 @@
+/***************************************************************************
+ *   Copyright (C) 2010 by Xavier Lefage                                   *
+ *   xavier.kwooty@gmail.com                                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
 #ifndef ITEMCHILDRENMANAGER_H
 #define ITEMCHILDRENMANAGER_H
 
 #include <QObject>
+#include <QModelIndex>
 #include "itemabstractupdater.h"
 #include "utility.h"
 using namespace UtilityNamespace;
 
 class CentralWidget;
-class StandardItemModel;
+class ItemParentUpdater;
 
 
 class ItemChildrenManager : public ItemAbstractUpdater {
@@ -20,9 +41,11 @@ public:
 
 private:
     CentralWidget* parent;
-    void setupConnections();
     bool displayIcons;
     bool smartPar2Download;
+
+    void setupConnections();
+
 
 signals:
     void downloadWaitingPar2Signal();
