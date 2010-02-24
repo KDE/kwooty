7c24996eebc7f1579d2fa15189cde3726930a3d0
Xavier <xavier.kwooty@gmail.com>
2010-02-24 21:43:34 +0100

    kwooty-0.3.1

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,7 @@ int main (int argc, char** argv)
 {
     KAboutData aboutData("kwooty",
                          "",
-                         ki18n("Kwooty"), "0.3.0",
+                         ki18n("Kwooty"), "0.3.1",
                          ki18n("Kwooty is a friendly nzb usenet binary downloader.\n It also supports automatic file repairing and archive extraction."),
                          KAboutData::License_GPL,
                          ki18n("Copyright (c) 2010 Xavier Lefage"),
