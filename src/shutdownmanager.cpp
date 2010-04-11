896f664d84363fe17a97f0fd2dfff4a28b92f76a
xavier.kwooty <xavier.kwooty@gmail.com>
2010-04-11 16:29:31 +0200

    Am/PM added

--- src/shutdownmanager.cpp
+++ src/shutdownmanager.cpp
@@ -151,7 +151,7 @@ void ShutdownManager::updateStatusBar() {
             dateTime = dateTime.addSecs(Settings::scheduleDateTime().time().hour() * 3600 +
                                         Settings::scheduleDateTime().time().minute() * 60);
 
-            shutdownMethodText = i18n("at") + ' ' + dateTime.toString("hh:mm");
+            shutdownMethodText = i18n("at") + ' ' + dateTime.toString(Utility::getSystemTimeFormat("hh:mm"));
         }
 
 
