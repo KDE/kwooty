5900526fdce1b04d7bbf31782c7d01a6c54723ce
Xavier <xavier.kwooty@gmail.com>
2010-09-29 23:11:44 +0200

    new file

---
+++ src/utilityiconpainting.cpp
@@ -0,0 +1,91 @@
+/***************************************************************************
+ *   Copyright (C) 2010 by Xavier Lefage                                   *
+ *   xavier.kwooty@gmail.com                                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+
+#include "utilityiconpainting.h"
+
+#include <KIcon>
+#include <KDebug>
+
+#include <QApplication>
+#include <QPainter>
+
+#include "preferences/preferencesserver.h"
+#include "utility.h"
+using namespace UtilityNamespace;
+
+
+
+UtilityIconPainting::UtilityIconPainting() : QObject(qApp){}
+
+UtilityIconPainting::~UtilityIconPainting() { kDebug();}
+
+
+UtilityIconPainting* UtilityIconPainting::instance = 0;
+UtilityIconPainting* UtilityIconPainting::getInstance() {
+
+    if (!instance) {
+        instance = new UtilityIconPainting();
+    }
+
+    return instance;
+
+}
+
+
+
+
+
+QPixmap UtilityIconPainting::blendOverLayTopRight(const QString& mainIconStr, const QString& overlayIconStr) {
+
+    QPixmap pixmap;
+
+    QString keyStr = mainIconStr + overlayIconStr;
+
+    if (UtilityIconPainting::getInstance()->textIconMap.contains(keyStr)) {
+
+        pixmap = UtilityIconPainting::getInstance()->textIconMap.value(keyStr);
+
+    }
+    else {
+
+        KIcon overlayIcon = KIcon(overlayIconStr);
+
+        if (!overlayIcon.isNull()) {
+
+
+            pixmap = KIconLoader::global()->loadIcon(mainIconStr, KIconLoader::Small, KIconLoader::SizeSmall);
+            QPainter p(&pixmap);
+
+            QPixmap overlayPixmap = overlayIcon.pixmap(10, 10);
+
+            p.drawPixmap(6, 0, overlayPixmap);
+            p.end();
+
+             UtilityIconPainting::getInstance()->textIconMap.insert(keyStr, pixmap);
+        }
+
+    }
+
+
+    return pixmap;
+}
+
+
---
+++ src/utilityiconpainting.h
@@ -0,0 +1,52 @@
+/***************************************************************************
+ *   Copyright (C) 2010 by Xavier Lefage                                   *
+ *   xavier.kwooty@gmail.com                                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+
+#ifndef UTILITYICONPAINTING_H
+#define UTILITYICONPAINTING_H
+
+
+#include <QObject>
+#include <QMap>
+#include <QPixmap>
+
+
+class UtilityIconPainting : public QObject {
+
+    Q_OBJECT
+
+public:
+    UtilityIconPainting();
+    ~UtilityIconPainting();
+
+    static UtilityIconPainting* getInstance();
+    QPixmap blendOverLayTopRight(const QString&, const QString&);
+
+
+private:
+    static UtilityIconPainting* instance;
+    QMap<QString, QPixmap> textIconMap;
+
+
+
+
+};
+
+#endif // UTILITYICONPAINTING_H
