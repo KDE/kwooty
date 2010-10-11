6a6dd4c1ecb04aaf5851bdfdde409cecae148ca3
xavier.kwooty <xavier.kwooty@gmail.com>
2010-10-11 23:12:33 +0200

    comment added

--- src/datarestorer.cpp
+++ src/datarestorer.cpp
@@ -45,7 +45,10 @@ DataRestorer::DataRestorer(CentralWidget* parent) : QObject (parent) {
 
 
     magicNumber = 0xC82F1D37;
+
+    // should be upgraded everytime a change is done in data stream :
     applicationVersion1 = 004;
+
     // map kwooty serialization version and its corresponding dataStream version
     versionStreamMap.insert(applicationVersion1, QDataStream::Qt_4_4);
 
