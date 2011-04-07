76246cca65cc2bc968677e73403abaf3aa130a6f
Xavier Lefage <xavier.kwooty@gmail.com>
2011-04-08 00:38:36 +0200

    do not try to extract if splitFile is already present

--- src/jobs/concatsplitfilesjob.cpp
+++ src/jobs/concatsplitfilesjob.cpp
@@ -96,67 +96,69 @@ bool ConcatSplitFilesJob::joinSplittedFiles() {
     bool joinFileExists = joinFile.exists();
 
     if (!joinFileExists) {
+
         joinFile.open(QIODevice::WriteOnly | QIODevice::Append);
-    }
 
-    // join splitted files (list of files is assumed to be ordered at this stage) :
-    foreach (const NzbFileData& currentNzbFileData, this->nzbFileDataList) {
+        // join splitted files (list of files is assumed to be ordered at this stage) :
+        foreach (const NzbFileData& currentNzbFileData, this->nzbFileDataList) {
 
-        fileProcessedNumber++;
+            fileProcessedNumber++;
 
-        // get current archive name :
-        QString archiveName = currentNzbFileData.getDecodedFileName();
+            // get current archive name :
+            QString archiveName = currentNzbFileData.getDecodedFileName();
 
-        QString fullPathArchiveName = this->fileSavePath + archiveName;
+            QString fullPathArchiveName = this->fileSavePath + archiveName;
 
-        // if file already exists, it means that it has been created during repair processing.
-        // in that case target file is already joined and this process does not have to be done :
-        if (!joinFileExists) {
+            // if file already exists, it means that it has been created during repair processing.
+            // in that case target file is already joined and this process does not have to be done :
+            if (!joinFileExists) {
 
-            // retrieve split file number :
-            bool conversionOk;
-            int splitFileNumber = QFileInfo(fullPathArchiveName).suffix().toInt(&conversionOk);
+                // retrieve split file number :
+                bool conversionOk;
+                int splitFileNumber = QFileInfo(fullPathArchiveName).suffix().toInt(&conversionOk);
 
-            // check that the file is a real splitted file and that the current splitted file
-            // is just following the previous one (eg : if .002 is missing, avoid to join .001, .003) :
-            if (!conversionOk ||
-                (fileProcessedNumber != splitFileNumber)) {
+                // check that the file is a real splitted file and that the current splitted file
+                // is just following the previous one (eg : if .002 is missing, avoid to join .001, .003) :
+                if (!conversionOk ||
+                        (fileProcessedNumber != splitFileNumber)) {
 
-                processFailed = true;
-                break;
+                    processFailed = true;
+                    break;
+                }
+
+
+                QFile currentSplitFile(fullPathArchiveName);
+
+                // open splitted file :
+                if (!currentSplitFile.open(QIODevice::ReadOnly)) {
+                    processFailed = true;
+                    break;
+                }
+
+                // write content in joined file :
+                if (joinFile.write(currentSplitFile.readAll()) == -1) {
+                    processFailed = true;
+                    break;
+                }
+
+                // close splitted file :
+                currentSplitFile.close();
             }
 
+            // emit progress percentage :
+            int progress = qRound(fileProcessedNumber * PROGRESS_COMPLETE / this->nzbFileDataList.size());
+            emit progressPercentSignal(progress, archiveName);
 
-            QFile currentSplitFile(fullPathArchiveName);
-
-            // open splitted file :
-            if (!currentSplitFile.open(QIODevice::ReadOnly)) {
-                processFailed = true;
-                break;
-            }
-
-            // write content in joined file :
-            if (joinFile.write(currentSplitFile.readAll()) == -1) {
-                processFailed = true;
-                break;
-            }
-
-            // close splitted file :
-            currentSplitFile.close();
         }
 
-        // emit progress percentage :
-        int progress = qRound(fileProcessedNumber * PROGRESS_COMPLETE / this->nzbFileDataList.size());
-        emit progressPercentSignal(progress, archiveName);
+        // job done, close file :
+        joinFile.close();
 
-    }
+        // if process failed, remove the incomplete joined file :
+        if (processFailed) {
+            joinFile.remove();
+        }
 
-    // job done, close file :
-    joinFile.close();
-
-    // if process failed, remove the incomplete joined file :
-    if (processFailed) {
-        joinFile.remove();
     }
 
     return processFailed;
